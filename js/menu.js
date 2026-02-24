f1a0dd94ee85f75a8932076f23e8e17dcbcac0a6
Gaston Furno <gastonfurno1@gmail.com>
2026-02-24 20:36:23 -0300

    corrigiendo errores

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,9 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navBar = document.querySelector(".nav-bar");
-const iconoMenu = document.getElementById("icono-menu");
-
 menuToggle.addEventListener("click", () => {
     navBar.classList.toggle("active");
 
     if (navBar.classList.contains("active")) {
-        iconoMenu.src = "/imagenes/close.png";
+        iconoMenu.src = iconoMenu.src.replace("open.png", "close.png");
     } else {
-        iconoMenu.src = "/imagenes/open.png";
+        iconoMenu.src = iconoMenu.src.replace("close.png", "open.png");
     }
 });
