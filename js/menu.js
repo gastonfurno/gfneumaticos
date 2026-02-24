36286769b9afdba5457117ea562fb4461a3bd7d8
Gaston Furno <gastonfurno1@gmail.com>
2026-02-24 20:48:37 -0300

    probando menu

--- js/menu.js
+++ js/menu.js
@@ -4,16 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const navBar = document.querySelector(".nav-bar");
     const iconoMenu = document.getElementById("icono-menu");
 
-    if (menuToggle) {
-        menuToggle.addEventListener("click", function () {
-            navBar.classList.toggle("active");
-
-            if (navBar.classList.contains("active")) {
-                iconoMenu.src = iconoMenu.src.replace("open.png", "close.png");
-            } else {
-                iconoMenu.src = iconoMenu.src.replace("close.png", "open.png");
-            }
-        });
+    if (!menuToggle || !navBar || !iconoMenu) {
+        console.log("Algún elemento no se encontró en esta página");
+        return;
     }
+
+    menuToggle.addEventListener("click", function () {
+        navBar.classList.toggle("active");
+
+        if (navBar.classList.contains("active")) {
+            iconoMenu.src = iconoMenu.src.replace("open.png", "close.png");
+        } else {
+            iconoMenu.src = iconoMenu.src.replace("close.png", "open.png");
+        }
+    });
 
 });
